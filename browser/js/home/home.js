7605270b2a24f4b83aaf33b15d2f24e0f446815f
Angela M. Choi <angela.minchoi@gmail.com>
2017-01-20 22:09:30 -0800

    commeted out google map for now

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -6,22 +6,6 @@ app.config(function ($stateProvider) {
 });
 
 app.controller('MainController', function($state, $scope, $log, $window, $timeout, MapFactory) {
-	$scope.markers = $window.markers;
-	$scope.deleteAMarker = MapFactory.deleteAMarker;
-
-	$scope.deleteAllMarkers = function() {
-		$window.days.forEach(function (day) {
-			day.forEach(function(marker) {
-				marker.setMap(null);
-			});
-		});
-	};
-
-	(function update() {
-		$timeout(update, 1);
-		$scope.markers = $window.markers;
-		$scope.days = $window.days;
-	}());
 
 });
 
