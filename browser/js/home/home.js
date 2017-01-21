aa41d09eb78ae2448b24d0c3d83440a52d849900
Angela M. Choi <angela.minchoi@gmail.com>
2017-01-21 01:29:13 -0800

    Clarifai api working & displays data

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -5,7 +5,22 @@ app.config(function ($stateProvider) {
     });
 });
 
-app.controller('MainController', function($state, $scope, $log, $window, $timeout, ImageFactory) {
-	$scope.submit = ImageFactory.submit;
+app.controller('MainController', function($state, $scope, $log, $window, $timeout, $http) {
+	$scope.dataFromClarifai = null;
+	$scope.showImage = function() {
+		if ($scope.image.url) return true;
+		return false;
+	}
+
+	$scope.submit = function(image) {
+        var imageUrl = image.url + '';
+        // predict the contents of an image by passing in a url
+        return $http.post('/api/images/clarifai', { imageUrl: imageUrl })
+        .then(function (response) {
+            $scope.image.url = image.url;
+            $scope.dataFromClarifai = response.data;
+            return response.data;
+        });
+    }
 });
 
--- server/app/routes/images/index.js
+++ server/app/routes/images/index.js
@@ -1,11 +1,9 @@
 'use strict';
 var router = require('express').Router();
-var rp =  require('request-promise');
 var Clarifai = require('clarifai');
 var config = require(__dirname + '/config.js').clarifi;
 module.exports = router;
 
-console.log('configs', config);
 // instantiate a new Clarifai app passing in your clientId and clientSecret
 
 var app = new Clarifai.App(config[0], config[1]);
@@ -14,13 +12,12 @@ router.get('/', function(req, res) {
     res.send('hey');
 });
 
-router.get('/clarifai', function (req, res) {
+router.post('/clarifai', function (req, res) {
     var imageUrl = req.body.imageUrl + '';
     // res.send('imgurl', imageUrl);
     app.models.predict(Clarifai.GENERAL_MODEL, imageUrl).then(
         function(response) {
-            console.log('response from ClarifaiAPI!', response);
-            res.send(response);
+            res.send(response.outputs[0].data.concepts);
         },
         function(err) {
             console.error('error from clarifi router', err);
